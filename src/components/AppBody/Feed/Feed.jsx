43eb60b3d4ee88e2345688a271956a726f02b871
John NoBody <devonly1984@hotmail.com>
2022-07-18 09:17:06 -0500

    2:05

--- src/components/AppBody/Feed/Feed.jsx
+++ src/components/AppBody/Feed/Feed.jsx
@@ -1,40 +1,46 @@
 import "./Feed.css";
 
 import {
-	CalendarViewDay,
-	Create,
-	EventNote,
-	Image,
-	Subscriptions,
+  CalendarViewDay,
+  Create,
+  EventNote,
+  Image,
+  Subscriptions,
 } from "@mui/icons-material";
-
+import Posts from "./Posts/Posts";
 import InputOptions from "./InputOptions/InputOptions";
 
 const Feed = () => {
-	const submitHandler = () => {};
-	return (
-		<div className="feed">
-			<div className="feed__inputContainer">
-				<div className="feed__input">
-					<Create />
-					<form onSubmit={submitHandler}>
-						<input type="text" />
-						<button type="submit">Send</button>
-					</form>
-				</div>
-				<div className="feed__inputOptions">
-					<InputOptions Icon={Image} title="Photo" color="#70B5F9" />
-					<InputOptions Icon={Subscriptions} title="Video" color="#E7A33E" />
-					<InputOptions Icon={EventNote} title="Event" color="#C0CBCD" />
-					<InputOptions
-						Icon={CalendarViewDay}
-						title="Write Article"
-						color="#7FC15E"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+  const submitHandler = () => {};
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <Create />
+          <form onSubmit={submitHandler}>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOptions Icon={Image} title="Photo" color="#70B5F9" />
+          <InputOptions Icon={Subscriptions} title="Video" color="#E7A33E" />
+          <InputOptions Icon={EventNote} title="Event" color="#C0CBCD" />
+          <InputOptions
+            Icon={CalendarViewDay}
+            title="Write Article"
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+      <Posts
+        name="John NoBody"
+        description="description"
+        message="Test Message"
+        photoUrl=""
+      />
+    </div>
+  );
 };
 
 export default Feed;
--- src/components/AppBody/Feed/Posts/Posts.jsx
+++ src/components/AppBody/Feed/Posts/Posts.jsx
@@ -1,7 +1,33 @@
+import { Avatar } from "@mui/material";
 import "./Posts.css";
-
-const Posts = () => {
-	return <div className="posts"></div>;
+import {
+  ThumbUpAltOutlined,
+  ChatOutlined,
+  ShareOutlined,
+  SendOutlined,
+} from "@mui/icons-material";
+import InputOptions from "../InputOptions/InputOptions";
+const Posts = ({ name, description, message, photoUrl }) => {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar src={photoUrl || null} />
+        <div className="post__info">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+      <div className="post__body">
+        <p>{message}</p>
+      </div>
+      <div className="post__buttons">
+        <InputOptions Icon={ThumbUpAltOutlined} title="Like" color="gray" />
+        <InputOptions Icon={ChatOutlined} title="Like" color="Chat" />
+        <InputOptions Icon={ShareOutlined} title="Like" color="Share" />
+        <InputOptions Icon={SendOutlined} title="Like" color="Send" />
+      </div>
+    </div>
+  );
 };
 
 export default Posts;
