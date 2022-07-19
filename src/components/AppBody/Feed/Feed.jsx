56fb9bd7c61df442c2fb87a117c23c2523d9e9d8
Bill White <soniclinuxknight@hotmail.com>
2022-07-19 06:49:50 -0500

    Attempt firebase

--- src/components/AppBody/Feed/Feed.jsx
+++ src/components/AppBody/Feed/Feed.jsx
@@ -1,46 +1,69 @@
 import "./Feed.css";
 
 import {
-  CalendarViewDay,
-  Create,
-  EventNote,
-  Image,
-  Subscriptions,
+	CalendarViewDay,
+	Create,
+	EventNote,
+	Image,
+	Subscriptions,
 } from "@mui/icons-material";
-import Posts from "./Posts/Posts";
+import { collection, getDocs, onSnapshot, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
 import InputOptions from "./InputOptions/InputOptions";
+import Posts from "./Posts/Posts";
+import { db } from "../../../config/firebase";
 
 const Feed = () => {
-  const submitHandler = () => {};
-  return (
-    <div className="feed">
-      <div className="feed__inputContainer">
-        <div className="feed__input">
-          <Create />
-          <form onSubmit={submitHandler}>
-            <input type="text" />
-            <button type="submit">Send</button>
-          </form>
-        </div>
-        <div className="feed__inputOptions">
-          <InputOptions Icon={Image} title="Photo" color="#70B5F9" />
-          <InputOptions Icon={Subscriptions} title="Video" color="#E7A33E" />
-          <InputOptions Icon={EventNote} title="Event" color="#C0CBCD" />
-          <InputOptions
-            Icon={CalendarViewDay}
-            title="Write Article"
-            color="#7FC15E"
-          />
-        </div>
-      </div>
-      <Posts
-        name="John NoBody"
-        description="description"
-        message="Test Message"
-        photoUrl=""
-      />
-    </div>
-  );
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const q = query(collection(db, "posts"));
+		const unsub = onSnapshot(q, (querySnapshot) => {
+			console.log(querySnapshot);
+			return setPosts(querySnapshot);
+		});
+		return unsub();
+	}, []);
+	const sendPost = (e) => {
+		e.preventDefault();
+		console.log(posts);
+	};
+	return (
+		<div className="feed">
+			<div className="feed__inputContainer">
+				<div className="feed__input">
+					<Create />
+					<form>
+						<input type="text" />
+						<button onClick={sendPost} type="submit">
+							Send
+						</button>
+					</form>
+				</div>
+				<div className="feed__inputOptions">
+					<InputOptions Icon={Image} title="Photo" color="#70B5F9" />
+					<InputOptions Icon={Subscriptions} title="Video" color="#E7A33E" />
+					<InputOptions Icon={EventNote} title="Event" color="#C0CBCD" />
+					<InputOptions
+						Icon={CalendarViewDay}
+						title="Write Article"
+						color="#7FC15E"
+					/>
+				</div>
+			</div>
+			{posts.map((post) => {
+				return (
+					<Posts
+						name={post.name}
+						message={post.message}
+						description={post.description}
+						photoUrl={post.photoUrl}
+					/>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Feed;
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDk4VimXVPVgYr1k23VXMCj8AzESHilPOs",
-  authDomain: "linkedin-yt-79554.firebaseapp.com",
-  projectId: "linkedin-yt-79554",
-  storageBucket: "linkedin-yt-79554.appspot.com",
-  messagingSenderId: "1030276691237",
-  appId: "1:1030276691237:web:65728f5babcbc8df8f96cc",
+	apiKey: "AIzaSyDk4VimXVPVgYr1k23VXMCj8AzESHilPOs",
+	authDomain: "linkedin-yt-79554.firebaseapp.com",
+	projectId: "linkedin-yt-79554",
+	storageBucket: "linkedin-yt-79554.appspot.com",
+	messagingSenderId: "1030276691237",
+	appId: "1:1030276691237:web:65728f5babcbc8df8f96cc",
 };
 const app = initializeApp(firebaseConfig);
-const db = getFirestore();
-const auth = getAuth();
-export { app, db, auth };
+const db = getFirestore(app);
+const auth = getAuth(app);
+export { db, auth };
